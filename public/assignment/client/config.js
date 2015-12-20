328206e4b80fac77bcdf3f8c69315c088f0d215e
Pengyi Song <songpeng@gmail.com>
2015-12-20 10:08:20 -0500

    assignment4

--- public/assignment/client/config.js
+++ public/assignment/client/config.js
@@ -9,30 +9,30 @@
     $routeProvider
       .when("/home",
       {
-        templateUrl: 'home/home.view.html'
+        templateUrl: 'views/home/home.view.html'
       })
       .when("/login",
       {
-        templateUrl: "login/login.view.html",
+        templateUrl: "views/login/login.view.html",
         controller: "LoginController"
       })
       .when("/register",
       {
-        templateUrl: "register/register.view.html",
+        templateUrl: "views/register/register.view.html",
         controller: "RegisterController"
       })
       .when("/profile",
       {
-        templateUrl: "profile/profile.view.html",
+        templateUrl: "views/profile/profile.view.html",
         controller: "ProfileController"
       })
      .when("/forms",
       {
-        templateUrl: "form/form.view.html",
+        templateUrl: "views/form/form.view.html",
         controller: "FormController"
       })
       .otherwise({
-        redirectTo: "home"
+        redirectTo: "views/home"
       })
   }
 })();
